3dc03320c09c02b0f194012cc70af9129c84248a
danoGuerrero <danielguerrerodev@gmail.com>
2023-08-28 17:50:47 -0600

    routes working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from "react";
-import OptionsButton from "./components/OptionsButton.jsx";
+import {
+  Route,
+  Routes,
+} from 'react-router-dom';
+import MainPage from "./pages/Main.jsx";
+import BestSellingSize from "./pages/BestSellingSize.jsx";
 
 const App = () => {
+	const routes = [
+		{
+			path: '/',
+			exact: false,
+			page: null,
+		},
+		{
+			path: '/pages/BestSellingSize',
+			exact: false,
+			page: <BestSellingSize />,
+		},
+	];
 	return (
-		<>
-			<OptionsButton />
-		</>
+		<div>
+			<MainPage />
+			<Routes>
+				{
+					routes.map((route, index) => (
+						<Route
+							element={route.page}
+							exact={route.exact}
+							key={index}
+							path={route.path}
+						/>
+					))
+				}
+			</Routes>
+		</div>
 	);
 }
 
